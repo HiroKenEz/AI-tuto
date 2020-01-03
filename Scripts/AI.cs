c28dba7d113efff24b62dbfcbb570b53366b143f
HiroKenEz <59478073+HiroKenEz@users.noreply.github.com>
2020-01-03 15:04:31 +0100

    Add files via upload

---
+++ Scripts/AI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+
+    public GameObject pt;
+    public GameObject BulletPrefab;
+    public Transform Firepoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 FinishPosition = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position, FinishPosition * 15, Color.red);
+
+            RaycastHit hit;
+        Ray ray = new Ray(transform.position, FinishPosition);
+
+        int layer_mask = LayerMask.GetMask("Default");
+        int layer_mask2 = LayerMask.GetMask("Player");
+        
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layer_mask, QueryTriggerInteraction.UseGlobal))
+        {
+
+
+        }
+        else
+        {
+            if (Physics.Raycast(ray, out hit, 25f, layer_mask2, QueryTriggerInteraction.UseGlobal))
+            {
+                GameObject bulletGo = (GameObject)Instantiate(BulletPrefab, Firepoint.position, Firepoint.rotation);
+                print("Ray hit by"+ hit.transform.name);
+                pt.transform.position = hit.point;
+
+            }
+        }
+
+
+    }
+}
---
+++ Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public Transform target; // update 2
+    public GameObject impactEffect; // update 2
+    public float speed = 70f;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = transform.TransformDirection(Vector3.forward);
+        float distancethisframe = speed * Time.deltaTime;
+        transform.Translate(dir.normalized * distancethisframe, Space.World);
+    }
+
+    void HitTarget() // update 2
+    {
+
+    }
+}
